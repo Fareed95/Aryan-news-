76725d9231c325650f5682da6ce4bafaae07dd3e
Fareed <fareedsayed95@gmail.com>
2024-05-11 19:30:38 +0530

    model to store article is ready (some more changes made)

--- frontend+backend/news/views.py
+++ frontend+backend/news/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
-
+from .models import News_Article
 # Create your views here.
 def pre_login_homepage(request):
     return render(request,'prelogin_homepage.html')
@@ -12,6 +12,13 @@ def pre_login_homepage(request):
 def HomePage(request):
     user = request.user
     # Pass the username to the template context
+    articlles = News_Article.objects.all()
+    
+    allarticle = []
+    catarticle= News_Article.objects.values('article_category','article_subcategory', 'id')
+    cats= {item["article_category"] for item in catarticle}
+    subcats= {item["article_subcategory"] for item in catarticle}
+    print(f"checking 1 : {catarticle}, checking2 = {cats} , checking3 = {subcats}")
     context = {'username': user.username}
     return render(request, 'homepage.html', context)
 
